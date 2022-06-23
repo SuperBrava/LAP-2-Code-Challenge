533859cb6ed9a87f0a9bf93e2e0e65a96b8c358a
Stu <stuart.judd.bsc@gmail.com>
2022-06-23 12:22:40 +0100

    code update for FE integration to pull BE data

--- client/static/js/requests.js
+++ client/static/js/requests.js
@@ -48,3 +48,34 @@ async function postBook(e){
 //         console.warn(err);
 //     }
 // }
+
+// creates a html elements and populates innertext with post data
+function postInstance(post){
+    const postContainer = document.createElement('div');
+    postContainer.className = "m-auto mt-5 col-lg-7 col-md-8 col-sm-10 post"
+    
+    const title = document.createElement('h4');
+    title.innerText = "title";
+    postContainer.append(title);
+
+    const author = document.createElement('h2');
+    author.innerText = post["author"];
+    postContainer.append(author);
+
+    const story = document.createElement('p');
+    story.className = "story";
+    story.innerText = post["story"];
+    postContainer.append(story);
+}
+
+
+function fetchLoading () {
+    fetch(`http://localhost:3000/posts`)
+        .then(r => r.json())
+        .then(r => {
+            r.data.forEach(element => postInstance(element));
+        })
+        .catch(console.warn);
+    };
+
+fetchLoading();
